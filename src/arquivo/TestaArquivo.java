92807b795ce707ca0acfb815800c4343b9764adc
Cassiano <schmitz.cassiano@gmail.com>
2021-03-23 21:52:04 -0300

    Update TestaArquivo.java

--- src/arquivo/TestaArquivo.java
+++ src/arquivo/TestaArquivo.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arquivo;
 
 import java.io.IOException;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author silvio.junior
- */
 public class TestaArquivo {
 
     public static void main(String[] args) throws IOException {
